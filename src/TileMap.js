03b75871f86c293d01ba52ba8a49f56d9d30e6d7
Nimai Leuenberger <leuenbergern2@bzz.ch>
2022-04-18 11:53:18 +0200

    Tilemap created with draw(), getPacman() and setCanvasSize() functions.

---
+++ src/TileMap.js
@@ -0,0 +1,68 @@
+import Pacman from "./Pacman.js";
+
+export default class TileMap {
+    constructor(tileSize) {
+        this.tileSize = tileSize;
+
+        this.yelloDot = new Image();
+        this.yelloDot.src = "../images/yellowDot.png";
+
+        this.wall = new Image();
+        this.wall.src = "../images/wall.png";
+    }
+
+    map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+
+
+    draw(ctx) {
+        for (let row = 0; row < this.map.length; row++) {
+            for (let column = 0; column < this.map[row].length; column++) {
+                let tile = this.map[row][column];
+                if (tile === 1) {
+                    ctx.drawImage(this.wall, column * this.tileSize, row * this.tileSize, this.tileSize, this.tileSize);
+                    //(image, x space to wall, y space to wall, image height, image width)
+                } else if (tile === 0) {
+                    ctx.drawImage(this.yelloDot, column * this.tileSize, row * this.tileSize, this.tileSize, this.tileSize);
+                }
+            }
+        }
+    }
+
+    getPacman() {
+        for (let row = 0; row < this.map.length; row++) {
+            for (let column = 0; column < this.map[row].length; column++) {
+                let tile = this.map[row][column];
+                if (tile === 3){
+                    this.map[row][column] = 0;
+                    return new Pacman(column*this.tileSize, row*this.tileSize, this.tileSize, this.map);
+                }
+            }
+        }
+    }
+
+    setCanvasSize(canvas) {
+        canvas.width = this.map[0].length * this.tileSize;
+        canvas.height = this.map.length * this.tileSize;
+    }
+}
