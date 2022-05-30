6aada7239ef984d189fbda271acf136369dfa8e1
Nimai Leuenberger <leuenbergern2@bzz.ch>
2022-05-30 14:20:13 +0200

    fonts added and changed TODO: give pacman 3 lives, reset him to original spawn add a retry button when game over

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const tileMap = new TileMap(tileSize);
 const pacman = tileMap.getPacman();
 const ghosts = tileMap.getGhost();
 const points = document.getElementById("points");
+let pacLives = 2;
 
 function game(){
     tileMap.draw(ctx);
@@ -22,15 +23,29 @@ function pause(){
 }
 
 function isGameOver(){
-    return !pacman.pinkDotActive && ghosts.some(
-        ghost => ghost.collideWith(pacman)
-    );
+    return !pacman.pinkDotActive && pacmanHasNoLives()
+}
+
+function pacmanHasNoLives(){
+    if (ghosts.some(ghost => ghost.collideWith(pacman))){
+        if (pacLives === 0){
+            return true;
+        } else {
+            pacLives--;
+            //tileMap.map[pacman.column / tileSize][pacman.row / tileSize] = 2;
+            tileMap.map[10][15] = 4;
+            console.log("collided");
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
 function drawGameOver(){
     if (isGameOver()){
         // rectangle:
-        ctx.rect(0, canvas.height / 2.7, canvas.width, 120);
+        ctx.rect(0, canvas.height / 2.66, canvas.width, 160);
         ctx.fillStyle = "black";
         ctx.fill();
         // border:
@@ -38,10 +53,11 @@ function drawGameOver(){
         ctx.strokeStyle = "white";
         ctx.stroke();
         // text:
-        ctx.font = "75px comic sans";
+        ctx.font = "60px pacfont";
+        ctx.fontFamily = "pacfont";
         ctx.fillStyle = "red";
         ctx.textAlign = "center";
-        ctx.fillText("Game Over!", canvas.width / 2, canvas.height / 2);
+        ctx.fillText("game over", canvas.width / 2, canvas.height / 2);
     }
 }
 
@@ -56,7 +72,8 @@ function isGameWon(){
         ctx.strokeStyle = "white";
         ctx.stroke();
         // text:
-        ctx.font = "75px comic sans";
+        ctx.font = "60px pacfont";
+        ctx.fontFamily = "pacfont";
         ctx.fillStyle = "yellow";
         ctx.textAlign = "center";
         ctx.fillText("Game Won!", canvas.width / 2, canvas.height / 2);
@@ -67,5 +84,3 @@ function isGameWon(){
 tileMap.setCanvasSize(canvas);
 // calling the game function 75 times per second (1000 ms)
 setInterval(game, 1000 / 100);
-
-
